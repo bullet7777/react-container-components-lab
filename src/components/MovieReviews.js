f0a31a863c4a170f5513f5a43661b79f295e7505
chris <chris.mahfouz@hotmail.com>
2021-04-24 12:55:41 +0300

    commit tree ce8e5376dc3709d79da7c9ed306576b92b385a79

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+
+
+const MovieReviews=({reviews})=>{
+    return(
+        <div className='review-list'>
+        {
+            reviews.map((obj)=>{
+                return (<div className='review'><h1>{obj.display_title}</h1></div>)
+            })
+        }
+        
+    </div>
+    )
+
+}
+
+export default MovieReviews
